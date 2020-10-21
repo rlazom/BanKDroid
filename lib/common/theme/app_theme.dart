68225c9fcab3b68784392c2266942d90034772b0
stark <stark@aleph.engineering>
2020-10-21 17:58:54 -0400

    Add app theme file

---
+++ lib/common/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    iconTheme: IconThemeData(
+      color: Colors.black87,
+    ),
+    textTheme: new TextTheme(
+      headline4: TextStyle(color: Colors.black87),
+      headline6: TextStyle(color: Colors.black87),
+      button: TextStyle(color: Colors.black54),
+      caption: TextStyle(color: Colors.black54),
+      subtitle1: TextStyle(color: Colors.black54),
+      bodyText1: TextStyle(color: Colors.black54),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    iconTheme: IconThemeData(
+      color: Colors.white54,
+    ),
+    textTheme: new TextTheme(
+      headline4: TextStyle(color: Colors.white70),
+      headline6: TextStyle(color: Colors.white70),
+      button: TextStyle(color: Colors.white60),
+      caption: TextStyle(color: Colors.white54),
+      subtitle1: TextStyle(color: Colors.white54),
+      bodyText1: TextStyle(color: Colors.white54),
+    ),
+  );
+}
