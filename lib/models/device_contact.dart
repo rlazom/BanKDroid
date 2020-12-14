329a323979eb0121a7b7ac3eba0430550d0e907e
Rene Lazo <stark@aleph.engineering>
2020-12-14 01:49:08 -0500

    Refactor Device Contact
    - Add new methods

--- lib/models/device_contact.dart
+++ lib/models/device_contact.dart
@@ -6,21 +6,41 @@ class DeviceContact {
   final String givenName;
   final String company;
   final String jobTitle;
-  final Iterable<Item> emails = [];
-  final Iterable<Item> phones = [];
+  final Iterable<Item> emails;
+  final Iterable<Item> phones;
   final Contact innerContact;
   Uint8List avatar;
+  bool loadedAvatar = false;
 
-  DeviceContact({this.displayName, this.givenName, this.company, this.jobTitle, this.innerContact});
+  DeviceContact({this.displayName, this.givenName, this.company, this.jobTitle, this.phones, this.emails, this.innerContact});
 
   String getAvatarLetter() {
-    return this.displayName[0];
+    return this.displayName[0].toUpperCase();
   }
 
-  Future<Uint8List> getAvatar({bool photoHighRes = false}) {
-    return ContactsService.getAvatar(
+  Future updateAvatar({bool photoHighRes = false}) async {
+    print('DeviceContact - updateAvatar()');
+    Uint8List image = await ContactsService.getAvatar(
       this.innerContact,
       photoHighRes: photoHighRes,
+    );
+    avatar = image;
+    loadedAvatar = true;
+  }
+
+  String getPhoneLabel(String phoneNumber) {
+    return phones.firstWhere((element) => element.value == phoneNumber).label;
+  }
+
+  static DeviceContact getDeviceContactFromContact(Contact c) {
+    return new DeviceContact(
+      displayName: c.displayName,
+      givenName: c.givenName,
+      company: c.company,
+      jobTitle: c.jobTitle,
+      phones: c.phones,
+      emails: c.emails,
+      innerContact: c
     );
   }
 }
