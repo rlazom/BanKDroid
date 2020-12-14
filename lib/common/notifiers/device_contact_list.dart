94c79db521e55f04b8c3d63ab5e822ffbbe67fcb
Rene Lazo <stark@aleph.engineering>
2020-12-14 01:47:54 -0500

    Refactor Device Contact List Notifier

--- lib/common/notifiers/device_contact_list.dart
+++ lib/common/notifiers/device_contact_list.dart
@@ -1,4 +1,5 @@
 import 'package:bankdroid/models/device_contact.dart';
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 
 class DeviceContactList extends ChangeNotifier {
@@ -6,8 +7,22 @@ class DeviceContactList extends ChangeNotifier {
 
   List<DeviceContact> get list => _list;
 
-  void addOperationList(List<DeviceContact> newList) {
-    this._list.addAll(newList);
+  Future<void> loadDeviceContactList() async {
+    print('DeviceContactList - loadDeviceContactList()');
+
+    this._list.clear();
+    Iterable<Contact> contacts = await ContactsService.getContacts(withThumbnails: false);
+    this._list = contacts.map((Contact e) => DeviceContact.getDeviceContactFromContact(e)).toList();
+    print('_list.length = ${_list.length}');
     notifyListeners();
+  }
+
+  DeviceContact getDeviceContactWithPhoneNumber({String phoneNumber}) {
+    print('getDeviceContactWithPhoneNumber($phoneNumber)');
+    return this._list.firstWhere((contact) => contact.phones.any((phone) => phone?.value == phoneNumber), orElse: () => null);
+  }
+
+  openContactForm() {
+    ContactsService.openContactForm();
   }
 }
